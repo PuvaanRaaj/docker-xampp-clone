6a1cd43a35c3b19c2f58ccbb848eb7a2111c027a
PuvaanRaaj <puvaan.shankar@fiuu.com>
2025-07-07 00:37:16 +0800

    Modify htdocs/index.php to call host.docker.internal:3000

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,19 @@
 <?php
+
 echo "<h2>Docker-XAMPP-Clone running PHP ".PHP_VERSION."</h2>";
-$pdo = new PDO('mysql:host=db;dbname=devdb', 'dev', 'secret');
-echo "<p>MariaDB connection OK!.</p>";
+
+// Try curl to host.docker.internal
+$url = 'http://host.docker.internal:3000';
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+
+if ($response === false) {
+    echo "<p style='color:red;'>Failed to connect to host.docker.internal:3000</p>";
+    echo "<pre>" . curl_error($ch) . "</pre>";
+} else {
+    echo "<p style='color:green;'>Success! Response from host.docker.internal:3000:</p>";
+    echo "<pre>" . htmlspecialchars(substr($response, 0, 200)) . "</pre>";
+}
+curl_close($ch);
